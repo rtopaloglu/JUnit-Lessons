5e2a3799f01733c6469b2bdc5b91c27804616e5f
Ridvan Topaloglu <navdirtopal@gmail.com>
2022-05-17 22:20:14 +0200

    17.05.22

--- src/test/java/day08_alerts/C04_IFrame.java
+++ src/test/java/day08_alerts/C04_IFrame.java
@@ -1,5 +1,6 @@
 package day08_alerts;
 
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Assert;
@@ -13,45 +14,52 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
 public class C04_IFrame {
-    // ● Bir class olusturun: IframeTest
-    //   ● https://the-internet.herokuapp.com/iframe adresine gidin.
-    //   ● Bir metod olusturun: iframeTest
-    //        ○ “An IFrame containing….” textinin erisilebilir oldugunu test edin ve  konsolda    yazdirin.
-    //        ○ Text Box’a “Merhaba Dunya!” yazin.
-    //        ○ TextBox’in altinda bulunan “Elemental Selenium”
-    //        linkini textinin gorunur oldugunu     dogrulayin ve  konsolda yazdirin.
-
+    //  ● Bir class olusturun: IframeTest
     WebDriver driver;
 
     @Before
-    public void setup() {
+    public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-
     }
+
     @After
     public void tearDown() {
-        //driver.close();
+        driver.close();
     }
 
     @Test
-    public void Test01() {
+    public void test01() throws InterruptedException {
+        //   ● https://the-internet.herokuapp.com/iframe adresine gidin.
         driver.get("https://the-internet.herokuapp.com/iframe");
+        //   ● Bir metod olusturun: iframeTest
+        //      ○ “An IFrame containing….” textinin erisilebilir oldugunu test edin
+        //      ve  konsolda    yazdirin.
         WebElement baslikElementi = driver.findElement(By.xpath("//h3"));
         Assert.assertTrue(baslikElementi.isEnabled());
         System.out.println(baslikElementi.getText());
-
+        //      ○ Text Box’a “Merhaba Dunya!” yazin.
+        // textbox'i dogru olarak locate etmemize ragmen driver bulamadi
+        // bunun uzerine HTML kodlari inceleyince
+        // textbox'in aslinda bir IFrame icerisinde oldugunu gorduk
+        // bu durumda once iframe'i locate edip
+        // switchTo() ile o iFrame'e gecmeliyiz
         WebElement iFrameElementi = driver.findElement(By.id("mce_0_ifr"));
         driver.switchTo().frame(iFrameElementi);
-
-        WebElement textKutusu = driver.findElement(By.xpath("//body[@id='tinymce']"));
-        textKutusu.clear();
-        textKutusu.sendKeys("Merhaba Dunya");
-
+        WebElement textkutusu = driver.findElement(By.xpath("//body[@id='tinymce']"));
+        textkutusu.clear();
+        textkutusu.sendKeys("Mehaba Dunya");
+        //      ○ TextBox’in altinda bulunan “Elemental Selenium”
+        //      linkinin textinin gorunur oldugunu  dogrulayin ve  konsolda yazdirin.
+        // link yazi elementini dogru locate etmemize ragmen yazdirmadi
+        // cunku yukarida iFrame'e gecis yapmistik
+        // once oradan cikmamiz lazim
         driver.switchTo().defaultContent();
-
-
+        WebElement linkYaziElementi = driver.findElement(By.linkText("Elemental Selenium"));
+        Assert.assertTrue(linkYaziElementi.isDisplayed());
+        System.out.println(linkYaziElementi.getText());
+        Thread.sleep(5000);
     }
 }
